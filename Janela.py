b1e844c7fc0d40ba970ea63764737d3a31247bfe
Joselito <motajoselito@gmail.com>
2019-07-26 18:04:31 -0300

    Removendo trechos de código não utilizado

--- Janela.py
+++ Janela.py
@@ -10,7 +10,6 @@ import tkinter.messagebox
 from functools import partial
 
 def adicionaNaLista():
-    #listbox.insert(END, nomeRepositorioEntry.get())
     entrada = nomeRepositorioEntry.get()
     if(entrada != ''):
         listbox.insert(END, entrada)
@@ -50,30 +49,6 @@ def extrairDados():
                         vRct, 
                         vLbs)
 
-    #app.deiconify()
-    """
-    folder = filedialog.askdirectory()
-    
-    if(folder != ""):
-        if(v.get() == 0):
-             extraction_data_per_repository(folder, listbox.get(0, END),
-                    varOpen.get(),
-                    varClose.get(),
-                    varComment.get(),
-                    varSummary.get(),
-                    varLabels.get())
-        elif(v.get() == 1):
-            extracting_and_save_separately(folder, listbox.get(0, END),
-                    varOpen.get(),
-                    varClose.get(),
-                    varComment.get(),
-                    varSummary.get(),
-                    varLabels.get())
-        """
-        #extractDataFromGithub(repoList, opFlag, clFlag, comFlag, evtFlag, rctFlag, labelsFlag)
-
-        
-
 def removerRepo():
     listbox.delete(listbox.curselection())
 
@@ -95,11 +70,6 @@ root.title("Github Fetcher")
 
 listbox = Listbox(root, height=20)
 listbox.place(x=452, y=30)
-
-#nomeRepositorioAdicionadoLabel = Label(
- #   root, text="Lista de Repositórios Adicionados", background=color)
-#nomeRepositorioAdicionadoLabel["font"] = ("Arial", "12", "bold")
-#nomeRepositorioAdicionadoLabel.place(x=395, y=5)
 
 nomeRepositorioLabel = Label(
     root, text="Insira o repositorio: Ex.: nomeUsuario/nomeRepositorio", background=color)
@@ -142,11 +112,6 @@ openCheck.place(x=12, y=150)
 closeCheck = Checkbutton(
     root, text="Closed", variable=varClose, background=color)
 closeCheck.place(x=12, y=170)
-"""
-summaryCheck = Checkbutton(
-    root, text="Extrair descrição", variable=varSummary, background=color)
-summaryCheck.place(x=102, y=150)
-"""
 
 labelsCheck = Checkbutton(
     root, text="Extrair labels do repositório", variable=varLabels, background=color)
@@ -164,27 +129,12 @@ commentCheck = Checkbutton(
     root, text="Extrair comentários", variable=varComment, background=color)
 commentCheck.place(x=102, y=210)
 
-#modoSalvamentoLabel = Label(
-   # root, text="Modo de salvamento das informações", background=color)
-#modoSalvamentoLabel["font"] = ("Arial", "12", "bold")
-#modoSalvamentoLabel.place(x=12, y=220)
-
-
-
-#v = IntVar()
-
-#modoUmRadio = Radiobutton(root, text="Issues em conjunto", variable=v, value=0, background=color)
-#modoUmRadio.place(x=12, y=250)
-#modoDoisRadio = Radiobutton(root, text="Issues separadas", variable=v, value=1, background=color)
-#modoDoisRadio.place(x=170, y=250)
-
 extrairButton = Button(root)
 extrairButton["text"] = "Extrair dados "
 extrairButton["font"] = ("Calibri", "10")
 extrairButton["width"] = 12
 extrairButton["command"] = extrairDados
 extrairButton.place(x=296, y=350)
-
 
 loginLabel = Label(
     root, text="Login: ", background=color)
@@ -204,43 +154,8 @@ passEntry = Entry(root)
 passEntry["width"] = 10
 passEntry.place(x=150, y=350)
 
-
-
-"""
-bugzillaButton = Button(root)
-bugzillaButton["text"] = "BugZilla "
-bugzillaButton["font"] = ("Calibri", "10")
-bugzillaButton["width"] = 12
-bugzillaButton["command"] = goBugZilla
-bugzillaButton.place(x=10, y=350)
-"""
-
-"""
-jiraButton = Button(root)
-jiraButton["text"] = "Jira"
-jiraButton["font"] = ("Calibri", "10")
-jiraButton["width"] = 12
-jiraButton["command"] = goJira
-jiraButton.place(x=120, y=350)
-"""
-
-#app = Tk()
-#app.title("Progresso")
-#app.resizable(False, False)
-#app.geometry("600x80+405+300")
-#app.configure(background=color)
-#app.withdraw()
-
-#updateLabel = Label(app, text = "Progresso da extração", background=color)
-#updateLabel.pack()
-#progressBar = ttk.Progressbar(app, orient=HORIZONTAL, mode='indeterminate')
-#progressBar.pack(expand=True, fill=BOTH, side=TOP)
-
 root.resizable(False, False)
 
 root.configure(background=color)
 
 root.mainloop() 
-
-#app.mainloop()
-
