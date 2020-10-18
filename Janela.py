154ef4a17345fd64bc4985b431ccfcd8428ac79d
Joselito Júnior <motajoselito@gmail.com>
2020-10-18 02:06:07 -0300

    Add labels to English

--- Janela.py
+++ Janela.py
@@ -73,7 +73,7 @@ listbox = Listbox(root, height=20)
 listbox.place(x=452, y=30)
 
 nomeRepositorioLabel = Label(
-    root, text="nomeUsuario/nomeRepositorio", background=color)
+    root, text="userName/repositoryName", background=color)
 nomeRepositorioLabel["font"] = ("Arial", "12", "bold")
 nomeRepositorioLabel.place(x=9, y=5)
 
@@ -82,14 +82,14 @@ nomeRepositorioEntry["width"] = 40
 nomeRepositorioEntry.place(x=10, y=26)
 
 inserirButton = Button(root)
-inserirButton["text"] = "Adicionar > "
+inserirButton["text"] = "ADD > "
 inserirButton["font"] = ("Calibri", "10")
 inserirButton["width"] = 12
 inserirButton["command"] = adicionaNaLista
 inserirButton.place(x=296, y=60)
 
 removerButton = Button(root)
-removerButton["text"] = "< Remover "
+removerButton["text"] = "< Remove "
 removerButton["font"] = ("Calibri", "10")
 removerButton["width"] = 12
 removerButton["command"] = removerRepo
@@ -115,19 +115,19 @@ closeCheck = Checkbutton(
 closeCheck.place(x=12, y=170)
 
 labelsCheck = Checkbutton(
-    root, text="Extrair labels do repositório", variable=varLabels, background=color)
+    root, text="Mine repository labels", variable=varLabels, background=color)
 labelsCheck.place(x=102, y=150)
 
 evtsCheck = Checkbutton(
-    root, text="Extrair eventos da issue", variable=evtsIssue, background=color)
+    root, text="Mine issue events", variable=evtsIssue, background=color)
 evtsCheck.place(x=102, y=170)
 
 reactionsCheck = Checkbutton(
-    root, text="Extrair reações de comentários", variable=varReactions, background=color)
+    root, text="Mine comments reactions", variable=varReactions, background=color)
 reactionsCheck.place(x=102, y=190)
 
 commentCheck = Checkbutton(
-    root, text="Extrair comentários", variable=varComment, background=color)
+    root, text="Mine comments", variable=varComment, background=color)
 commentCheck.place(x=102, y=210)
 
 languageLabel = Label(root, text="Database language key:", background=color)
@@ -137,7 +137,7 @@ languageCombo.current(1)
 languageCombo.place(x=102, y=260)
 
 extrairButton = Button(root)
-extrairButton["text"] = "Extrair dados "
+extrairButton["text"] = "Mine repos"
 extrairButton["font"] = ("Calibri", "10")
 extrairButton["width"] = 12
 extrairButton["command"] = extrairDados
